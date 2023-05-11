3ff525a26a254ace3b56023940cb3227c0d6a311
makastorff <132590214+makastorff@users.noreply.github.com>
2023-05-11 15:37:59 -0500

    Update streamlit_app.py
    lesson 9 - normalize json file and output it to screen as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# write your own comment -what does the next line do?  -> normalizes json version
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?  -> outputs to screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
